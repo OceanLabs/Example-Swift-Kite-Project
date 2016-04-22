991877b4554b8a1d3b25e6a1440c15374d6fb054
Deon Botha <deon@dbotha.com>
2016-04-22 10:16:06 +0100

    Initial Commit

---
+++ SwiftApplePayTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftApplePayTest
+//
+//  Created by Deon Botha on 22/04/2016.
+//  Copyright © 2016 Deon Botha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
